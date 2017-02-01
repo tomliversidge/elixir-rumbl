df3324d91e629c07ba21c22fe9959c399108d43a
Tom Liversidge <tom@liversidge.me>
2017-02-01 22:43:25 +0200

    add users page

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,21 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repository
+  """
+  def all(Rumbl.User) do
+    [
+      %Rumbl.User{id: "1", name: "John", username: "john", password: "foo"}
+    ]
+  end
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
 end
---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,7 @@
+defmodule Rumbl.UserController do
+  use Rumbl.Web, :controller
+  def index(conn, _params) do
+    users = Repo.all(Rumbl.User)
+    render conn, "index.html", users: users
+  end
+end
