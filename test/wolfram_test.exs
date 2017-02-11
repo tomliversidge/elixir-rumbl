f219a6f633966a227ba389a4a55e52e825d5af5f
Tom Liversidge <tom@liversidge.me>
2017-02-11 21:14:29 +0200

    upping timeout for receiving messages in tests

--- test/wolfram_test.exs
+++ test/wolfram_test.exs
@@ -6,13 +6,13 @@ defmodule Rumbl.WolframTest do
     ref = make_ref()
     {:ok, pid} = Wolfram.start_link("1 + 1", ref, self(), 1)
     Process.monitor(pid)
-    assert_receive {:results, ^ref, [%Rumbl.InfoSys.Result{text: "2"}]}
+    assert_receive {:results, ^ref, [%Rumbl.InfoSys.Result{text: "2"}]}, 2500, :assert_receive_timeout
     assert_receive {:DOWN, _ref, :process, ^pid, :normal}
   end
 
   test "no query results reports an empty list" do
     ref = make_ref()
     {:ok, _} = Wolfram.start_link("none", ref, self(), 1)
-    assert_receive {:results, ^ref, []}
+    assert_receive {:results, ^ref, []}, 2500, :assert_receive_timeout
   end
 end
